d01084fb02627e2de13c65a415f0cd6f967211a6
07faizanali <mohd.faizanali.m.z@gmail.com>
2025-03-11 13:54:09 +0530

    updated build.sh and requirement.txt

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect Static Files
 python manage.py collectstatic --no-input
 
+# Run Migrations
 python manage.py migrate
 
-if[[ $CREATE_SUPERUSER ]];
-then
+# Create Superuser (if enabled)
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi  
+fi
